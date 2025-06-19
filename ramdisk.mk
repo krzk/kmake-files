5beb8fc463151a19e6ef1f95b99ec64dc5dd8f36
Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
2025-06-19 13:53:36 +0200

    ramdisk: Split ramdisk creation to separate include file for re-use
    Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>

---
+++ ramdisk.mk
@@ -0,0 +1,13 @@
+# SPDX-License-Identifier: GPL-2.0
+
+modules_cpio_gz: dir-pkg
+	@echo Building $(BUILD)modules.cpio.gz
+	@mkdir -p $(BUILD)tar-install/usr/
+	@mv $(BUILD)tar-install/lib $(BUILD)tar-install/usr/lib
+	@cd $(BUILD)tar-install/; find usr | cpio -o -H newc -R +0:+0 |gzip -c > $(BUILD)modules.cpio.gz
+	@cd -
+
+ramdisk: modules_cpio_gz
+	@echo Appending modules.cpio.gz to final ramdisk
+	@cat $(INITRD) $(BUILD)modules.cpio.gz \
+		> $(BUILD)ramdisk.cpio.gz;
